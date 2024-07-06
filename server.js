d3b79f38d437c2471b6d5e278e38069a4f9f6898
rajeshbiz4 <rajesh.pandhare181@gmail.com>
2024-07-06 20:57:01 +0530

    deleted setting

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ var app = express();
 
 // const dotenv = require("dotenv");
 // dotenv.config();
-var authenticateToken = require("./middleware/authenticateToken.js");
-var user_controller = require("./modules/user/userController.js");
-var auth_Controller = require("./modules/auth/authController.js");
-var branch_Controller = require("./modules/branch/branchController.js");
-var image_controller = require("./modules/images/imageController.js");
+// var authenticateToken = require("./middleware/authenticateToken.js");
+// var user_controller = require("./modules/user/userController.js");
+// var auth_Controller = require("./modules/auth/authController.js");
+// var branch_Controller = require("./modules/branch/branchController.js");
+// var image_controller = require("./modules/images/imageController.js");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-app.use("/login", auth_Controller);
-app.use("/user", user_controller);
-app.use("/branch", branch_Controller);
-app.use("/images", image_controller);
+// app.use("/login", auth_Controller);
+// app.use("/user", user_controller);
+// app.use("/branch", branch_Controller);
+// app.use("/images", image_controller);
 
 app.listen(port, () => {
   console.log("Server listening on port " + port);
